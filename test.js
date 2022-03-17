6c3573ad42b7500ef73aac4cf693a584fd53ce74
antoniocasino <casinoantonio@gmail.com>
2022-03-17 15:22:03 +0100

    update temporal unit

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ import { strict as assert } from 'assert';
 import _ from "lodash";
 
 let groupAndTemporalSortCollection = groupAndTemporalSort(
-    {collection:testData,unit:temporalUnit.DAY,temporalOrder:sortOrder.descending,
-        temporalPropertyName:"date",groupPropertyName:"group-1",groupOrder:sortOrder.ascending});        
+    {collection:testData,unit:temporalUnit.MINUTE,temporalOrder:sortOrder.descending,
+        temporalPropertyName:"date",groupPropertyName:"group-1",groupOrder:sortOrder.ascending});       
 
 assert(groupAndTemporalSortCollection.flatMap(e=>e.entries).length == testData.length, "the collection size is not the same size");
